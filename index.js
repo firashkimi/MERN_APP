0bade9562c6528b5b2da82eb1738b4fe47adc0e5
unknown <firashkim709@gmail.com>
2022-10-03 12:48:14 +0100

    first route

--- index.js
+++ index.js
@@ -5,14 +5,8 @@ const app = express()
 
 //First Route 
 app.get('/api/goals',(req,res)=>{
-    //function accepts a single parameter body that describe the body which is to be sent in the response
-    //It returns an Object
-    // res.status(200).send('Get Goals')
-
-    // function sends a JSON response. This method sends a response (with the correct content-type)
-    //that is the parameter converted to a JSON string using the JSON.stringify()method.
+    res.status(200).send('Get Goals')
     res.status(200).json({message:'Get Goals'})
-
 })
 
 
